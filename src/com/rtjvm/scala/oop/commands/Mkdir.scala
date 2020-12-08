fcc4d693de7642ed9f29108f2e3e20c40de2a056
JuanCalMun <juancalmun@gmail.com>
2020-12-08 14:05:46 +0100

    Mkdir uncompleted

--- src/com/rtjvm/scala/oop/commands/Mkdir.scala
+++ src/com/rtjvm/scala/oop/commands/Mkdir.scala
@@ -1,20 +1,11 @@
 package com.rtjvm.scala.oop.commands
 
-import com.rtjvm.scala.oop.files.Directory
+import com.rtjvm.scala.oop.files.{DirEntry, Directory}
 import com.rtjvm.scala.oop.filesystem.State
 
 class Mkdir(newFileName: String) extends Command {
 
-  def printAllreadyExist(fileName: String): String = s"Entry $fileName already exists!"
-
   def printCreatedDir(fileName: String): String = s"$fileName created successfully"
-
-  def printIllegalCreate(fileName: String): String = s"$fileName must not contain separators"
-
-  def checkIllegal(newFileName: String): Boolean =
-    newFileName.contains(Directory.SEPARATOR) || newFileName.contains(".")
-
-  def execMkdir(): State = ???
 
   override def apply(state: State): State = {
     val workingDirectory = state.workingDirectory
@@ -23,13 +14,50 @@ class Mkdir(newFileName: String) extends Command {
     else if (checkIllegal(newFileName))
       state.setMessage(printIllegalCreate(newFileName))
     else {
-      execMkdir()
+      execMkdir(state, newFileName)
     }
 
   }
+
+  def printAllreadyExist(fileName: String): String = s"Entry $fileName already exists!"
+
+  def printIllegalCreate(fileName: String): String = s"$fileName must not contain separators"
+
+  def checkIllegal(newFileName: String): Boolean =
+    newFileName.contains(Directory.SEPARATOR) || newFileName.contains(".")
+
+  def execMkdir(state: State, name: String): State = {
+    val workingDirectory = state.workingDirectory
+
+    def updateStructure(currentDirectory: Directory, path: List[String], newEntry: DirEntry): Directory = {
+      if (path.isEmpty) currentDirectory.addEntry(newEntry)
+      else {
+        val oldEntry = currentDirectory.findEntry(path.head).asDirectory
+        currentDirectory.replaceEntry(oldEntry.name, updateStructure(oldEntry, path.tail, newEntry))
+      }
+    }
+
+    /*
+    *   1.  Get all the directories in the full path
+    */
+    val allDirsInPath = workingDirectory.getAllFoldersInPath
+
+    /*
+    *   2.  Create new directory entry in the working directory
+    */
+    val newDir = Directory.empty(workingDirectory.path, name)
+
+    /*
+    *   3.  Update the whole directory structure starting from the root (The directory structure is immutable)
+    */
+    val newRoot = updateStructure(state.root, allDirsInPath, newDir)
+
+    /*
+    *   4.  Find the new working directory instance given working directory full path, in the new directory structure
+    */
+    val newWorkingDirectory = newRoot.findDescendant(allDirsInPath)
+
+    State(newRoot, newWorkingDirectory)
+  }
+
 }
-
-
-
-
-
--- src/com/rtjvm/scala/oop/files/Directory.scala
+++ src/com/rtjvm/scala/oop/files/Directory.scala
@@ -2,8 +2,23 @@ package com.rtjvm.scala.oop.files
 
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
-  def hasEntry(name: String): Boolean = contents.contains(name)
+  def hasEntry(name: String): Boolean = findEntry(name) != null
 
+  def findEntry(entryName: String): DirEntry = {
+    contents.find(e => e.name.eq(entryName)).orNull
+  }
+
+  def addEntry(newEntry: DirEntry): Directory =
+    new Directory(parentPath, name, newEntry :: contents)
+
+  def getAllFoldersInPath: List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList
+
+  def findDescendant(path: List[String]): Directory = ???
+
+  def replaceEntry(entryName: String, newEntry: DirEntry): Directory = ???
+
+  override def asDirectory: Directory = this
 }
 
 object Directory {
